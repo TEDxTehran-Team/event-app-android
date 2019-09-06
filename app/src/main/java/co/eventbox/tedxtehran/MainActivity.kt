bcedf3296d5f9fc4e1e6dec59cc07e8db9d1c2fb
farshidroohi <farshid.roohi.a@gmail.com>
2019-09-06 22:59:26 +0430

    start redesign TEDxTehran :)

---
+++ app/src/main/java/co/eventbox/tedxtehran/MainActivity.kt
@@ -0,0 +1,12 @@
+package co.eventbox.tedxtehran
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
