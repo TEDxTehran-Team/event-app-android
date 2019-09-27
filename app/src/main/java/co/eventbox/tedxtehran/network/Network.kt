a722a93231a6fff060de8321b25d561148c92083
farshidroohi <farshid.roohi.a@gmail.com>
2019-09-27 13:08:00 +0330

    bottom navigation and fragments

---
+++ app/src/main/java/co/eventbox/tedxtehran/network/Network.kt
@@ -0,0 +1,9 @@
+package co.eventbox.tedxtehran.network
+
+/**
+ * Created by Farshid Roohi.
+ * TEDxTehran | Copyrights 2019-09-09.
+ */
+class Network {
+
+}
---
+++ app/src/main/java/co/eventbox/tedxtehran/view/activities/MainActivity.kt
@@ -0,0 +1,17 @@
+package co.eventbox.tedxtehran.view.activities
+
+import android.os.Bundle
+import co.eventbox.tedxtehran.R
+
+/**
+ * Created by Farshid Roohi.
+ * TEDxTehran | Copyrights 2019-09-06.
+ */
+
+class MainActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
