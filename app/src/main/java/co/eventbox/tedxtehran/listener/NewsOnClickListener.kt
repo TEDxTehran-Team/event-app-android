d9f1a5ff12f2cf3851cc69427110fad267bf70c1
mahdi darvishi <mahdidrv78@gmail.com>
2020-11-20 02:29:09 +0330

    open news url in browser and set placeholder

---
+++ app/src/main/java/co/eventbox/tedxtehran/listener/NewsOnClickListener.kt
@@ -0,0 +1,9 @@
+package co.eventbox.tedxtehran.listener
+
+/**
+ * Created by Mahdi Darvishi.
+ * TEDxTehran | Copyrights 11/20/20.
+ */
+interface NewsOnClickListener {
+    fun onSelected(position: Int, url: String)
+}
--- app/src/main/java/co/eventbox/tedxtehran/view/news/NewsFragment.kt
+++ app/src/main/java/co/eventbox/tedxtehran/view/news/NewsFragment.kt
@@ -1,6 +1,7 @@
 package co.eventbox.tedxtehran.view.news
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,7 +9,12 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import co.eventbox.tedxtehran.R
+import co.eventbox.tedxtehran.listener.NewsOnClickListener
+import co.eventbox.tedxtehran.model.LinkType
 import co.eventbox.tedxtehran.utilities.gone
+import co.eventbox.tedxtehran.utilities.openBrowser
+import co.eventbox.tedxtehran.utilities.toImageURL
+import co.eventbox.tedxtehran.utilities.toPair
 import co.eventbox.tedxtehran.viewModel.NewsViewModel
 import kotlinx.android.synthetic.main.fragment_news.*
 
@@ -16,7 +22,7 @@ import kotlinx.android.synthetic.main.fragment_news.*
  * Created by Farshid Roohi.
  * TEDxTehran | Copyrights 2019-09-26.
  */
-class NewsFragment : Fragment() {
+class NewsFragment : Fragment(), NewsOnClickListener {
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,7 +35,7 @@ class NewsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val adapter = NewsAdapter()
+        val adapter = NewsAdapter(this)
         this.recyclerViewNews.adapter = adapter
 
         val viewModel = ViewModelProvider(this).get(NewsViewModel::class.java)
@@ -40,4 +46,10 @@ class NewsFragment : Fragment() {
         })
 
     }
+
+    override fun onSelected(position: Int, url: String) {
+        context?.openBrowser(url)
+    }
+
+
 }
