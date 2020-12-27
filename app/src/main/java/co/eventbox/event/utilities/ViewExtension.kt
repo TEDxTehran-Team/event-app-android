740f57ec3b9534f19e04be69df2c097c43d8454e
mahdi darvishi <mahdidrv78@gmail.com>
2020-12-28 01:10:35 +0330

    add some method in ViewExtension

--- app/src/main/java/co/eventbox/event/utilities/ViewExtension.kt
+++ app/src/main/java/co/eventbox/event/utilities/ViewExtension.kt
@@ -1,9 +1,12 @@
 package co.eventbox.event.utilities
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.view.View
+import androidx.fragment.app.Fragment
+import co.eventbox.event.view.activities.MainActivity
 
 /**
  * Created by Farshid Roohi.
@@ -17,7 +20,26 @@ fun View.visible() {
     this.visibility = View.VISIBLE
 }
 
-fun Context.openBrowser(url:String?){
+fun Fragment.showBottomNav() {
+    (activity as MainActivity).showBottomNav()
+}
+
+fun Fragment.hideBottomNav() {
+    (activity as MainActivity).hideBottomNav()
+}
+
+fun Fragment.hideAppbar() {
+    (activity as MainActivity).hideAppBar()
+}
+
+fun Fragment.showAppbar() {
+    (activity as MainActivity).showAppBar()
+}
+
+
+
+
+fun Context.openBrowser(url: String?) {
     val intent = Intent(Intent.ACTION_VIEW)
     intent.data = Uri.parse(url)
     startActivity(intent)
