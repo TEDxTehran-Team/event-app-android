a80ba9f05c96d883eeff68d9183198f541ab4aac
mahdi darvishi <mahdidrv78@gmail.com>
2020-12-27 22:46:17 +0330

    Multi language and setLocate

--- app/src/main/java/co/eventbox/event/view/activities/MainActivity.kt
+++ app/src/main/java/co/eventbox/event/view/activities/MainActivity.kt
@@ -1,12 +1,16 @@
 package co.eventbox.event.view.activities
 
 import android.content.Intent
+import android.content.res.Configuration
+import android.content.res.Resources
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import co.eventbox.event.R
 import co.eventbox.event.utilities.setupWithNavController
 import co.eventbox.event.view.about.AboutUsActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
 
 /**
  * Created by Farshid Roohi.
@@ -17,6 +21,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        locate("en")
         setContentView(R.layout.activity_main)
 
         val navigations = listOf(
@@ -36,5 +41,17 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this, AboutUsActivity::class.java))
         }
 
+
     }
+
+    fun locate(locate: String) {
+        val locale = Locale(locate)
+        Locale.setDefault(locale)
+        val resources: Resources = resources
+        val configuration: Configuration = resources.getConfiguration()
+        configuration.locale = locale
+        resources.updateConfiguration(configuration, resources.getDisplayMetrics())
+    }
+
+
 }
