bfcd955f385b9d9dac88da9bc0e6b73bc166fdf2
Farshid Roohi <Farshid.roohi.a@Gmail.com>
2021-03-07 22:16:52 +0330

    Revert "Revert "Cleanups""

--- app/src/main/java/co/eventbox/event/Config.kt
+++ app/src/main/java/co/eventbox/event/Config.kt
@@ -1,15 +1,10 @@
 package co.eventbox.event
 
-import android.util.LayoutDirection
 import android.view.View
 
 
 object Config {
-
-
-    // 0 for ltr and 1 for rtl
     const val direction = View.LAYOUT_DIRECTION_LTR
     const val language = "en"
-
-
+    const val token = "56fe1591-c5c1-4113-aba3-73fca1f5aacd" // English Data Token
 }
