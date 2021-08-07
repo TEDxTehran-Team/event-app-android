02882eca96ebef793866549adc4087c2ec6c2474
mahdi darvishi <mahdidrv78@gmail.com>
2021-08-07 13:24:04 +0430

    create register, verify fragments

---
+++ app/src/main/java/co/eventbox/event/view/newtworking/auth/RegisterFragment.kt
@@ -0,0 +1,30 @@
+package co.eventbox.event.view.newtworking.auth
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import co.eventbox.event.R
+
+/**
+ * Created by Farshid Roohi.
+ * TEDxTehran | Copyrights 2019-09-26.
+ */
+class RegisterFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_register, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+    }
+}
